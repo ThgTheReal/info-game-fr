b32006ba846ca64bc7f02cd26306c34fa91f904f
ThgTheReal <thg75557@gmail.com>
2026-02-05 11:11:46 +0100

    stamina added

--- Player/CatPlayer.gd
+++ Player/CatPlayer.gd
@@ -5,13 +5,17 @@ class_name Player
 @export var health = 100
 @export var stamina = 100
 
-
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var RayL = $ClimbL
 @onready var RayR = $ClimbR
+
+
+
+
+
+
 
 func _ready() -> void:
 	pass
@@ -58,6 +62,20 @@ func _physics_process(delta: float) -> void:
 #		$AnimatedSprite2D.stop()
 #	
 
+
+
+func useStamina():
+	if IsKlettern == false:
+		stamina =+ 1
+		await get_tree().create_timer(0.5).timeout
+	
+	if Klettern:
+		stamina =- 10
+		await get_tree().create_timer(1.0).timeout
+
+
+var  IsKlettern = false
+
 func Klettern():
 	if stamina < 0:
 		pass
@@ -65,10 +83,12 @@ func Klettern():
 	if not is_on_floor():
 		if (RayL.is_colliding()  or RayR.is_colliding()) and Input.is_action_pressed("Jump"):
 			velocity.y = -350
-		#elif RayL.is_colliding() or RayR.is_colliding() and !Input.is_action_pressed("Jump"):
-		#	velocity.y = 0
-		
-
+			IsKlettern = true
+		elif RayL.is_colliding() or RayR.is_colliding() and !Input.is_action_pressed("Jump"):
+			velocity.y = 0
+			IsKlettern = true
+	if is_on_floor():
+		IsKlettern = false
 
 
 #var EnemieBody:Enemie = null
@@ -77,13 +97,15 @@ func Klettern():
 #	pass
 
 
-#func CheckIfEnemieInAtack(body: Node2D) -> void:
+func CheckIfEnemieInAtack(body: Node2D) -> void:
+	pass
 #	if body is Enemie:
 #		EnemieBody = body
 
 
 
 
-#func EnemieLeaveArea(body: Node2D) -> void:
+func EnemieLeaveArea(body: Node2D) -> void:
+	pass
 #	if body is Enemie:
 #		EnemieBody = null
