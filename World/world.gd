b4938ddc2aceeb7e6667d59c2799028905dbbedf
Behrchen69 <discoardacc@gmail.com>
2026-03-12 10:39:48 +0100

    texture

---
+++ World/world.gd
@@ -0,0 +1,13 @@
+extends Node2D
+#
+
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	var PlayerInstance:Player = preload("res://Player/CatPlayer.tscn").instantiate()
+	
+	add_child(PlayerInstance)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
