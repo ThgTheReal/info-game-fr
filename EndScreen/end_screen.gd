a1b60b0035d5fed11008ab7ec1b7bfed08238372
ThgTheReal <thg75557@gmail.com>
2026-04-16 14:35:56 +0200

    Replay Button

--- EndScreen/end_screen.gd
+++ EndScreen/end_screen.gd
@@ -9,3 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_redo_pressed() -> void:
+	GlobalVariables.RatKills = 0
+	GlobalVariables.Round = 0
+	GlobalVariables.comunismRatHealth = 0
+	GlobalVariables.comunismRatMaxHealth = 0
+	get_tree().change_scene_to_file("res://World/WorldElements/StartWorld.tscn")
